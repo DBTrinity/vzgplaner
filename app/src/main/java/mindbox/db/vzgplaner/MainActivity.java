3b398d398ac16ac0cbe97e7d21f573489437ccbe
HenrySachs <henry.sachs@deutschebahn.com>
2018-12-08 00:13:48 +0100

    add geojson load

--- app/src/main/java/mindbox/db/vzgplaner/MainActivity.java
+++ app/src/main/java/mindbox/db/vzgplaner/MainActivity.java
@@ -2,22 +2,30 @@ package mindbox.db.vzgplaner;
 
 import android.app.Activity;
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.util.Log;
 
 import org.osmdroid.api.IMapController;
 import org.osmdroid.bonuspack.kml.KmlDocument;
-import org.osmdroid.bonuspack.kml.StyleSelector;
+import org.osmdroid.bonuspack.kml.Style;
 import org.osmdroid.config.Configuration;
-import org.osmdroid.tileprovider.tilesource.ITileSource;
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
-import org.osmdroid.tileprovider.tilesource.XYTileSource;
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapView;
 import org.osmdroid.views.overlay.FolderOverlay;
 
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
-import java.util.HashMap;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.StringWriter;
+import java.io.UnsupportedEncodingException;
+import java.io.Writer;
 
 public class MainActivity extends Activity {
     MapView map = null;
@@ -46,26 +54,23 @@ public class MainActivity extends Activity {
         map.setBuiltInZoomControls(true);
         map.setMultiTouchControls(true);
 
-        final ITileSource tileSource = new XYTileSource(
-                "Mapnik", 1, 18, 256, ".png", new String[]{"https://openmaptiles.github.io/positron-gl-style"}, "");
-        map.setTileSource(tileSource);
 
         KmlDocument kmlDocument = new KmlDocument();
+        String jsonString = loadJSONFromAsset(ctx);
+        kmlDocument.parseGeoJSON(jsonString);
 
-
-        InputStream inputStream = getResources().openRawResource(R.raw.railway_station_nodes);
-        kmlDocument.parseKMLStream(inputStream, null);
-
-        FolderOverlay kmlOverlay = (FolderOverlay)kmlDocument.mKmlRoot.buildOverlay(map, null, null,kmlDocument);
-        map.getOverlays().add(kmlOverlay);
-
+        Drawable defaultMarker = getResources().getDrawable(R.drawable.marker_default);
+        Bitmap defaultBitmap = ((BitmapDrawable) defaultMarker).getBitmap();
+        Style defaultStyle = new Style(defaultBitmap, 0x901010AA, 5f, 0x20AA1010);
+        FolderOverlay geoJsonOverlay = (FolderOverlay) kmlDocument.mKmlRoot.buildOverlay(map, defaultStyle, null, kmlDocument);
 
         IMapController mapController = map.getController();
         mapController.setZoom(9.5);
-        GeoPoint startPoint = new GeoPoint(48.8583, 2.2944);
+        GeoPoint startPoint = new GeoPoint(	52.520008, 	13.404954);
         mapController.setCenter(startPoint);
 
-
+        map.getOverlays().add(geoJsonOverlay);
+        map.invalidate();
     }
 
     public void onResume() {
@@ -84,5 +89,37 @@ public class MainActivity extends Activity {
         //SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
         //Configuration.getInstance().save(this, prefs);
         map.onPause();  //needed for compass, my location overlays, v6.0.0 and up
+    }
+
+    public String loadJSONFromAsset(Context context) {
+        InputStream is = getResources().openRawResource(R.raw.railwaystationnodes);
+        Writer writer = new StringWriter();
+        char[] buffer = new char[1024];
+        try {
+            Reader reader = null;
+            try {
+                reader = new BufferedReader(new InputStreamReader(is, "UTF-8"));
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+            int n;
+            try {
+                while ((n = reader.read(buffer)) != -1) {
+                    writer.write(buffer, 0, n);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } finally {
+            try {
+                is.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        String jsonString = writer.toString();
+        Log.d("jsonString", jsonString);
+        return jsonString;
     }
 }
